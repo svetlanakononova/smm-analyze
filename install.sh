51eae6c629cc2a50c60baaa0d46ddd6263572f99
svetlanakononova <svetlana.kononova@gmail.com>
2023-03-26 23:00:13 +0300

    modify install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,12 @@ rm Anaconda3-2023.03-Linux-x86_64.sh
 
 sudo apt-get -y update
 sudo apt-get -y install docker.io
+
+
+mkdir bin
+cd bin
+wget https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -O docker-compose
+chmod +x docker-compose
+cd 
+echo 'export PATH="${HOME}/bin:${PATH}"' >>  ~/.bashrc
+
