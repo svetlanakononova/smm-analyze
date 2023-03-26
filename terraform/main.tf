588ab4e0b9f59f89e9dbee06b81f7dbc46ceac00
svetlanakononova <svetlana.kononova@gmail.com>
2023-03-27 01:21:24 +0300

    -

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,8 @@ terraform {
 }
 
 provider "google" {
-  project = "${project}"
-  region = "${region}"
+  project = var.project
+  region = var.region
   // credentials = file(var.credentials)  # Use this if you do not want to set env-var GOOGLE_APPLICATION_CREDENTIALS
 }
 
