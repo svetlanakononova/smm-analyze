fd2b8fd54db93f9886fd30ad02121efc22494c99
svetlanakononova <svetlana.kononova@gmail.com>
2023-03-26 22:51:21 +0300

    modify install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ wget https://repo.anaconda.com/archive/Anaconda3-2023.03-Linux-x86_64.sh
 chmod +x Anaconda3-2023.03-Linux-x86_64.sh
 ./Anaconda3-2023.03-Linux-x86_64.sh
 rm Anaconda3-2023.03-Linux-x86_64.sh
+
+#Install docker and docker-compose
+
+sudo apt-get update
+sudo apt-get install docker.io
