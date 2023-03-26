2670d286900c5939dbc39350152991a8ff301d61
Svetlana Kononova <svetlana.kononova@gmail.com>
2023-03-26 22:18:32 +0300

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#Install Anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2023.03-Linux-x86_64.sh
+chmod +x Anaconda3-2023.03-Linux-x86_64.sh
+./Anaconda3-2023.03-Linux-x86_64.sh
+rm Anaconda3-2023.03-Linux-x86_64.sh
